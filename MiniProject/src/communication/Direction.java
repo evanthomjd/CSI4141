6d7fee2d19a628fd6d647bff2a6b3fded4f1b8c4
evan <evanthomjd@gmail.com>
2013-03-16 16:04:59 -0400

    Serialized the direction class so it can be passed as part of an intent

--- MiniProject/src/communication/Direction.java
+++ MiniProject/src/communication/Direction.java
@@ -1,10 +1,21 @@
 package communication;
 
-public class Direction {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Direction implements Parcelable {
 	private String direction;
 	private double lat;
 	private double log;
 	
+	public Direction(){
+		
+	}
+	public Direction(Parcel source){
+		lat = source.readDouble();
+		log = source.readDouble();
+		direction = source.readString();
+	}
 	
 	public String getDirection() {
 		return direction;
@@ -24,6 +35,32 @@ public class Direction {
 	public void setLog(double log) {
 		this.log = log;
 	}
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeDouble(lat);
+		dest.writeDouble(log);
+		dest.writeString(direction);
+		
+	}
+	
+	
+	public static final Parcelable.Creator<Direction> CREATOR
+    = new Parcelable.Creator<Direction>() 
+   {
+         public Direction createFromParcel(Parcel source) 
+         {
+             return new Direction(source);
+         }
+
+         public Direction[] newArray (int size) 
+         {
+             return new Direction[size];
+         }
+    };
 	
 	
 	
